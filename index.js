c147d2a9a132abd6a6b03416597da17fe5b13f5c
Roshannair24 <70207465+Roshannair24@users.noreply.github.com>
2021-09-22 19:44:06 +0530

    Feat : updated js

--- index.js
+++ index.js
@@ -6,18 +6,35 @@ let checkNotes = document.querySelector("#check-notes");
 
 checkNotes.addEventListener("click", clickHandler);
 
+function hascharacter(myString) {
+  return /[a-z]/.test(myString);
+}
+
 function clickHandler() {
   console.log("in handler");
 
-  console.log(billAmount);
   console.log(billAmount.value);
 
-  console.log(cashGiven);
   console.log(cashGiven.value);
 
+  if (hascharacter(billAmount.value)) {
+    console.log(hascharacter(billAmount.value));
+  
+    console.log("Invalid entry at billAmount.");
+  }else if(hascharacter(cashGiven.value)){
+    console.log(hascharacter(cashGiven.value));
+  
+    console.log("Invalid entry at cashGiven.");
+
+  }else{
+
+
+// guarunteed its a number now
 
 
 
 
+  }
 
+  //   clickHandler end
 }
