237139584c04031dc0e363eae76db211df0d360e
Roshannair24 <70207465+Roshannair24@users.noreply.github.com>
2021-09-23 11:32:28 +0530

    Feat  : updated conditionals

--- index.js
+++ index.js
@@ -1,40 +1,68 @@
 let billAmount = document.querySelector("#bill-amount");
 
+let outercashcontainer = document.querySelector(".outer-cash-container");
+
 let cashGiven = document.querySelector("#cash-given");
 
 let checkNotes = document.querySelector("#check-notes");
 
-checkNotes.addEventListener("click", clickHandler);
+let smallPara = document.querySelector(".small-para");
+let smallParaCash = document.querySelector(".small-para-cash");
 
 function hascharacter(myString) {
   return /[a-z]/.test(myString);
 }
 
-function clickHandler() {
+billAmount.addEventListener("change", changehandler);
+
+function changehandler() {
   console.log("in handler");
 
   console.log(billAmount.value);
 
-  console.log(cashGiven.value);
-
   if (hascharacter(billAmount.value)) {
     console.log(hascharacter(billAmount.value));
-  
+
     console.log("Invalid entry at billAmount.");
-  }else if(hascharacter(cashGiven.value)){
-    console.log(hascharacter(cashGiven.value));
-  
-    console.log("Invalid entry at cashGiven.");
+    smallPara.classList.add("show");
+    outercashcontainer.classList.remove("show");
+  } else {
+    smallPara.classList.remove("show");
+    // valid billamount
+    // show cash given
 
-  }else{
+    outercashcontainer.classList.add("show");
 
-
-// guarunteed its a number now
-
-
-
-
+    console.log(cashGiven.value);
   }
+}
 
-  //   clickHandler end
+cashGiven.addEventListener("change", changehandlercash);
+
+function changehandlercash() {
+  console.log("in cash handler");
+
+  console.log(cashGiven.value);
+
+  if (hascharacter(cashGiven.value)) {
+    console.log(hascharacter(cashGiven.value));
+
+    console.log("Invalid entry at Cash given.");
+    smallParaCash.classList.add("show");
+  } else {
+    smallParaCash.classList.remove("show");
+    // valid cash amount
+
+    console.log(cashGiven.value);
+  }
+}
+
+checkNotes.addEventListener("click", checknotesclickHandler);
+
+function checknotesclickHandler() {
+  if (hascharacter(billAmount.value) || hascharacter(cashGiven.value)) {
+    console.log("invalid entry somewhere");
+  } else {
+    console.log("all valid");
+  }
 }
