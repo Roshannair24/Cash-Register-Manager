ea2ef8fa6a542509732baa6e798da29a85b34809
Roshannair24 <70207465+Roshannair24@users.noreply.github.com>
2021-09-23 13:43:45 +0530

    feat: Added more js

--- index.js
+++ index.js
@@ -4,10 +4,26 @@ let outercashcontainer = document.querySelector(".outer-cash-container");
 
 let cashGiven = document.querySelector("#cash-given");
 
+
+
+
 let checkNotes = document.querySelector("#check-notes");
+
+
+
+let tableHolder = document.querySelector(".table-holder");
+
+
+
 
 let smallPara = document.querySelector(".small-para");
 let smallParaCash = document.querySelector(".small-para-cash");
+let smallParaButton = document.querySelector(".small-para-button ");
+
+let validnumeral = null;
+
+let changeToBeReturned = null;
+let arrofDenominations = [2000, 500, 100, 50, 20, 10, 5, 1];
 
 function hascharacter(myString) {
   return /[a-z]/.test(myString);
@@ -26,6 +42,12 @@ function changehandler() {
     console.log("Invalid entry at billAmount.");
     smallPara.classList.add("show");
     outercashcontainer.classList.remove("show");
+    validnumeral = false;
+  } else if (billAmount.value <= 0) {
+    console.log("Invalid entry at billAmount.");
+    smallPara.classList.add("show");
+    outercashcontainer.classList.remove("show");
+    validnumeral = false;
   } else {
     smallPara.classList.remove("show");
     // valid billamount
@@ -34,6 +56,7 @@ function changehandler() {
     outercashcontainer.classList.add("show");
 
     console.log(cashGiven.value);
+    validnumeral = true;
   }
 }
 
@@ -49,20 +72,64 @@ function changehandlercash() {
 
     console.log("Invalid entry at Cash given.");
     smallParaCash.classList.add("show");
+    validnumeral = false;
+  } else if (cashGiven.value <= 0) {
+    console.log("Invalid entry at Cash given.");
+    smallParaCash.classList.add("show");
+    validnumeral = false;
   } else {
     smallParaCash.classList.remove("show");
     // valid cash amount
 
     console.log(cashGiven.value);
+    validnumeral = true;
   }
 }
 
 checkNotes.addEventListener("click", checknotesclickHandler);
 
 function checknotesclickHandler() {
-  if (hascharacter(billAmount.value) || hascharacter(cashGiven.value)) {
+  if (validnumeral === false) {
     console.log("invalid entry somewhere");
   } else {
-    console.log("all valid");
+    console.log("all valid numerals");
+
+    //process the values
+
+    if (cashGiven.value < billAmount.value) {
+      console.log("bill:" + billAmount.value);
+      console.log("cash:" + cashGiven.value);
+      console.log("There isnt enough cash");
+
+      smallParaButton.classList.add("show");
+    } else {
+      smallParaButton.classList.remove("show");
+      console.log("there is enough cash");
+
+
+      tableHolder.classList.add("show");
+
+
+      changeToBeReturned = cashGiven.value - billAmount.value;
+
+      console.log("changeToBeReturned:" + changeToBeReturned);
+
+      returnChangeInMinNotes(changeToBeReturned) ;
+
+    }
   }
+}
+
+
+
+function returnChangeInMinNotes(change){
+
+
+  console.log("in return change func");
+
+
+
+
+  
+
 }
